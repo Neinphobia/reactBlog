3b83d89ec287c6238eeccafad333874e80f3a288
Furkan <furkangonulal.dev@gmail.com>
2023-07-22 13:55:46 +0300

    api changed to the vercel->

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setPending(true)
-        const result = await axios.get("https://api-y38j.onrender.com/blogs");
+        setPending(true);
+        const result = await axios.get(
+          "https://api-for-react-neinphobia.vercel.app/blogs"
+        );
         setData(result.data);
         setPending(false);
       } catch (error) {
@@ -28,18 +30,24 @@ const App = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      setPending(true)
-      await axios.post("https://api-y38j.onrender.com/blogs", { name, body , img , ytlink });
+      setPending(true);
+      await axios.post("https://api-for-react-neinphobia.vercel.app/blogs", {
+        name,
+        body,
+        img,
+        ytlink,
+      });
       setName("");
       setBody("");
       setImg("");
       setYtlink("");
-      
-      const result = await axios.get("https://api-y38j.onrender.com/blogs");
-      
-      setPending(false)
-      setData(result.data);
 
+      const result = await axios.get(
+        "https://api-for-react-neinphobia.vercel.app/blogs"
+      );
+
+      setPending(false);
+      setData(result.data);
     } catch (error) {
       console.error(error);
     }
@@ -47,36 +55,41 @@ const App = () => {
 
   const handleDelete = async (id) => {
     try {
-      setPending(true)
-      await axios.delete(`https://api-y38j.onrender.com/blogs/${id}`);
-      const result = await axios.get("https://api-y38j.onrender.com/blogs");
+      setPending(true);
+      await axios.delete(
+        `https://api-for-react-neinphobia.vercel.app/blogs/${id}`
+      );
+      const result = await axios.get(
+        "https://api-for-react-neinphobia.vercel.app/blogs"
+      );
       setData(result.data);
-      setPending(false)
+      setPending(false);
     } catch (error) {
       console.error(error);
     }
   };
-  
+
   return (
     <div className="container">
-      
       <form onSubmit={handleSubmit} className="form">
         <label>
-          <input type="text"
-          value={img}
-          onChange={(event) => setImg(event.target.value)}
-          className="input"
-          placeholder="Lütfen fotoğrafınızın linkini giriniz."
-           />
+          <input
+            type="text"
+            value={img}
+            onChange={(event) => setImg(event.target.value)}
+            className="input"
+            placeholder="Lütfen fotoğrafınızın linkini giriniz."
+          />
         </label>
         <br />
         <label>
-          <input type="text"
-          value={ytlink}
-          onChange={(event) => setYtlink(event.target.value)}
-          className="input"
-          placeholder="Lütfen youtube videosu linkini giriniz."
-           />
+          <input
+            type="text"
+            value={ytlink}
+            onChange={(event) => setYtlink(event.target.value)}
+            className="input"
+            placeholder="Lütfen youtube videosu linkini giriniz."
+          />
         </label>
         <br />
         <label>
@@ -100,46 +113,51 @@ const App = () => {
             placeholder="Bir mesaj giriniz"
           />
         </label>
-      
+
         <br />
         <button type="submit" className="button">
           Veri Ekle
         </button>
       </form>
-     
+
       <h1 className="title">Gelen Veriler:</h1>
       <ul className="data-list">
         {pending && (
-          
           <div className="lds-ring">
-            <span className="yukleniyor">Yükleniyor..</span> 
+            <span className="yukleniyor">Yükleniyor..</span>
             <div></div>
             <div></div>
             <div></div>
             <div></div>
           </div>
-        ) }
-        {data.map(item => (
+        )}
+        {data.map((item) => (
           <li key={item._id} className="data-item">
             <div className="item-header">
-              
-              {item.img&&item.img.includes("https://") ? <img src={`${item.img}`} alt="yüklenemedi veya fotoğraf yok."/> : null }
+              {item.img && item.img.includes("https://") ? (
+                <img src={`${item.img}`} alt="yüklenemedi veya fotoğraf yok." />
+              ) : null}
 
-               <br  />
+              <br />
 
-               {item.ytlink&&item.ytlink.includes("youtu") ? <a href={`${item.ytlink}`} target="_blank">{item.ytlink}</a> : null}
-               
+              {item.ytlink && item.ytlink.includes("youtu") ? (
+                <a href={`${item.ytlink}`} target="_blank">
+                  {item.ytlink}
+                </a>
+              ) : null}
+
               <h2 className="item-title">{item.name}</h2>
               <p className="item-body">{item.body}</p>
-              
 
-              {!pending && 
-              <button
-                onClick={() => handleDelete(item._id)}
-                className="delete-button"
-              > Veriyi Sil</button>
-            }
-
+              {!pending && (
+                <button
+                  onClick={() => handleDelete(item._id)}
+                  className="delete-button"
+                >
+                  {" "}
+                  Veriyi Sil
+                </button>
+              )}
             </div>
           </li>
         ))}
